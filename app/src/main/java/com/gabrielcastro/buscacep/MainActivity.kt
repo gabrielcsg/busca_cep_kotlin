f549f34ac7d1588b45766a39070e6697fff085a1
Gabriel Castro <gabrielcssg@gmail.com>
2021-08-19 17:34:13 -0300

    error text and open map

--- app/src/main/java/com/gabrielcastro/buscacep/MainActivity.kt
+++ app/src/main/java/com/gabrielcastro/buscacep/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.gabrielcastro.buscacep
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -13,6 +17,13 @@ import com.google.android.material.textfield.TextInputEditText
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var tvLoading: TextView
+    private lateinit var tvError: TextView
+    private lateinit var btnOpenMap: Button
+
+    private lateinit var street: String
+    private lateinit var neighborhood: String
+    private lateinit var city: String
+    private lateinit var state: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,50 +35,81 @@ class MainActivity : AppCompatActivity() {
 
         val btnSearch = findViewById<Button>(R.id.id_btn_search)
         tvLoading = findViewById(R.id.id_tv_loading)
+        tvError = findViewById(R.id.id_tv_errorText)
+        btnOpenMap = findViewById(R.id.id_btn_map)
 
         btnSearch.setOnClickListener {
             val inputCep = findViewById<TextInputEditText>(R.id.id_textinput_cep)
             when {
-                inputCep.text.toString().isEmpty() -> Toast.makeText(
-                    applicationContext,
-                    getString(R.string.error_empty),
-                    Toast.LENGTH_SHORT
-                ).show()
-                inputCep.text.toString().length != 8 -> Toast.makeText(
-                    applicationContext,
-                    getString(R.string.error_length),
-                    Toast.LENGTH_SHORT
-                ).show()
+                inputCep.text.toString().isEmpty() -> {
+                    tvError.text = getString(R.string.error_empty)
+                    tvError.visibility = View.VISIBLE
+                }
+                inputCep.text.toString().length != 8 -> {
+                    tvError.text = getString(R.string.error_length)
+                    tvError.visibility = View.VISIBLE
+                }
                 else -> getCep(inputCep.text.toString())
             }
         }
+
+        btnOpenMap.setOnClickListener {
+            val geolocation = Uri.parse(
+                "geo:0,0?q=${street.replace(" ", "+")}%2C+${
+                    neighborhood.replace(" ", "+")
+                }"
+            )
+            showMap(geolocation)
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun getCep(cep: String) {
         tvLoading.visibility = View.VISIBLE
         viewModel.getAddress(cep)
         viewModel.myResponse.observe(this, { response ->
-            tvLoading.visibility = View.INVISIBLE
+            tvLoading.visibility = View.GONE
             if (response.isSuccessful) {
                 if (response.body()?.error == true) {
-                    Toast.makeText(
-                        applicationContext,
-                        getString(R.string.error_invalid_cep),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    tvError.text = getString(R.string.error_invalid_cep)
+                    tvError.visibility = View.VISIBLE
                 } else {
-                    findViewById<TextView>(R.id.id_tv_street).text = response.body()?.street!!
-                    findViewById<TextView>(R.id.id_tv_neighborhood).text = response.body()?.neighborhood!!
-                    findViewById<TextView>(R.id.id_tv_city).text = response.body()?.city!!
-                    findViewById<TextView>(R.id.id_tv_state).text = response.body()?.state!!
+                    tvError.visibility = View.GONE
+                    btnOpenMap.visibility = View.VISIBLE
+                    street = response.body()?.street!!
+                    neighborhood = response.body()?.neighborhood!!
+                    city = response.body()?.city!!
+                    state = response.body()?.state!!
+                    findViewById<TextView>(R.id.id_tv_street).text =
+                        getString(R.string.street) + street
+                    findViewById<TextView>(R.id.id_tv_neighborhood).text =
+                        getString(R.string.neighborhood) + neighborhood
+                    findViewById<TextView>(R.id.id_tv_city).text = getString(R.string.city) + city
+                    findViewById<TextView>(R.id.id_tv_state).text =
+                        getString(R.string.state) + state
                     return@observe
                 }
             } else Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
 
-            findViewById<TextView>(R.id.id_tv_street).text = ""
-            findViewById<TextView>(R.id.id_tv_neighborhood).text = ""
-            findViewById<TextView>(R.id.id_tv_city).text = ""
-            findViewById<TextView>(R.id.id_tv_state).text = ""
+            clearFields()
         })
+    }
+
+    private fun clearFields() {
+        findViewById<TextView>(R.id.id_tv_street).text = ""
+        findViewById<TextView>(R.id.id_tv_neighborhood).text = ""
+        findViewById<TextView>(R.id.id_tv_city).text = ""
+        findViewById<TextView>(R.id.id_tv_state).text = ""
+        btnOpenMap.visibility = View.INVISIBLE
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun showMap(geoLocation: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = geoLocation
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
